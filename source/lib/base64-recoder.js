2d3606285ebf9835716d407029a44cad9764ad5b
UniQMG <>
2020-06-24 22:28:32 -0700

    Fix encoder, again

--- source/lib/base64-recoder.js
+++ source/lib/base64-recoder.js
@@ -1,15 +1,6 @@
-// This is javascript's fault for not having a useful base64 encoding function
-// Adapted from https://developer.mozilla.org/en-US/docs/Web/API/WindowOrWorkerGlobalScope/btoa#Unicode_strings
+// Javascript, please, I beg of you, JUST OFFER A USEFUL ENCODING FUNCTION FFS
+// (To better understand this message, look at the history of this file)
 function b64Recode(value) {
-  let string = JSON.stringify(value);
-  let binary = String.fromCharCode(
-    ...new Uint8Array(
-      new Uint16Array(string.length).map((_,i) => {
-        return string.charCodeAt(i);
-      }).buffer
-    )
-  );
-  let safeString = btoa(binary);
-
-  return `JSON.parse(String.fromCharCode(...new Uint16Array(new Uint8Array([...atob("${safeString}")].map(v => v.charCodeAt(0))).buffer)))`;
+  let enc = JSON.stringify([...JSON.stringify(value)].map(char => char.charCodeAt(0)));
+  return `JSON.parse(JSON.parse("${enc}").map(char => String.fromCharCode(char)).join(''))`;
 }
